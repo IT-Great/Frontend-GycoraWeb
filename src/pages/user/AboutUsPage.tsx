5743fdc4c24f2b0e740d851980e1b6570a66f5f8
Sean Alden <sean.alden@great.sch.id>
2026-05-25 11:03:47 +0700

    update about us page bg color

--- src/pages/user/AboutUsPage.tsx
+++ src/pages/user/AboutUsPage.tsx
@@ -785,7 +785,7 @@ export default function AboutUsPage() {
   }, [location]);
 
   return (
-    <div className="min-h-screen pb-24 overflow-hidden font-sans bg-gray-300">
+    <div className="min-h-screen pb-24 overflow-hidden font-sans bg-gray-100">
       {/* ================= HEADER SECTION (HERO) ================= */}
       <div className="relative bg-[#006A4E] py-32 text-center px-4 overflow-hidden">
         {/* Dekorasi Latar Belakang */}
