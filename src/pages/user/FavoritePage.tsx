a9fad6528ad0dcc88abbb3789f1260d357123e95
Sean Alden <sean.alden@great.sch.id>
2026-06-03 16:46:21 +0700

    update favorite page

--- src/pages/user/FavoritePage.tsx
+++ src/pages/user/FavoritePage.tsx
@@ -180,7 +180,7 @@ export default function FavoritePage() {
               return (
                 <Link 
                   key={item.id} 
-                  to={`/products/${item.product.slug}`} 
+                  to={`/product/${item.product.slug}`} 
                   className="relative flex flex-col overflow-hidden transition-all duration-300 bg-white border border-transparent group rounded-2xl hover:shadow-xl hover:border-gycora/20"
                 >
                   {/* Product Image Area */}
